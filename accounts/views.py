450050353a8e0eac972e9c95cbab1cee376d47b0
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-08-23 16:19:38 +0100

    Created dashboard view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .forms import CustomUserCreationForm
