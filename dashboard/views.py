349f6c6bf5acde793f6edf1be2110fc5137fdfee
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-08-30 08:30:36 +0100

    Resolve issue with `refresh` view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+
 
 from results.models import SemesterResult
 # Create your views here.
@@ -9,16 +11,29 @@ from results.models import SemesterResult
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
 
+@login_required
 def refresh(request):
     user = request.user
     user_results = list(user.results.order_by('result_id').all())
     prev_courses = list()
+
+    def is_allowed(course, result_id):
+        """Determines the courses that are allowed for cgpa calculation and
+        handles complications with 100L Engineering results."""
+        return (str(course.ca) != 'nan') and (not(result_id.endswith('100E')) or course.code.startswith('GNS'))
+    
     for result in user_results:
-        prev_courses += [(course.gradient, course.unit) for course in result.courses.all()]
-        gradients, units = list(zip(*prev_courses))
+        result_id = result.result_id
+        prev_courses += [(course.gradient, course.unit) for course in result.courses.all() if is_allowed(course, result_id)]
+        try:
+            gradients, units = list(zip(*prev_courses))
+        except ValueError:
+            # if `prev_courses` is empty i.e. none of the course passes validation
+            gradients, units = (), ()   # defaults to empty tuple
         result.cgpa = sum(gradients)/ sum(units)
+    
     user.cgpa = result.cgpa
     user.save()
     SemesterResult.objects.bulk_update(user_results, fields=('cgpa',))
 
-    return dashboard(request)
+    return redirect(reverse('dashboard:dashboard'))
