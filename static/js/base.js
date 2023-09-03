47e2e8a951c7b8e4a633ecc9db04c7a7f906b6fa
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-09-03 03:52:45 +0100

    😎🐱‍👤: Template styling and population
    Added the hide/show password button
    Added the hide/show cgpa button
    Styled the above buttons

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,13 @@
 const hamMenu = document.querySelector('.hamburger-menu');
 const myNav = document.getElementById("mobile-nav");
+
+const alertMsgs = document.querySelectorAll('.alert');
+const hidePwds = document.querySelectorAll('.hide-pwd')
+const cgpa = document.querySelector('#cgpa');
+const hide = document.querySelector('#hide');
+
+
+// hamburger menu
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     if (myNav.style.height == "100%") {
@@ -8,4 +16,41 @@ hamMenu.addEventListener('click', () => {
     else {
         myNav.style.height = "100%";
     };
+});
+
+// timeout for each alert messages
+// also removes them on click
+alertMsgs.forEach((msg) => {
+    setTimeout(() => {
+        msg.style.display = "none";
+    }, 3000);
+    msg.addEventListener('click', () => {
+        msg.style.display = "none";
+    });
+});
+
+// Hide/Show password button
+hidePwds.forEach((icon) => {
+    icon.addEventListener('click', () => {
+        inputTag = icon.previousElementSibling;
+        imgs = icon.children;
+        if (inputTag.type === "text") {
+            inputTag.type = "password";
+        } else {
+            inputTag.type = "text";
+        };
+        imgs[0].classList.toggle('no-show');
+        imgs[1].classList.toggle('no-show');
+    });
+});
+
+// Hide/Show cgpa
+hide.addEventListener('click', () => {
+    hideChildren = hide.children;
+    cgpaChildren = cgpa.children;
+
+    hideChildren[0].classList.toggle('no-show');
+    hideChildren[1].classList.toggle('no-show');
+    cgpaChildren[0].classList.toggle('no-show');
+    cgpaChildren[1].classList.toggle('no-show');
 });
