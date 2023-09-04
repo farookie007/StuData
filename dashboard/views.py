498da466d30d6e6e9197872d835b43e4295f9e9a
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-09-04 14:17:42 +0100

    Added some more comments

--- dashboard/views.py
+++ dashboard/views.py
@@ -13,6 +13,8 @@ def dashboard(request):
 
 @login_required
 def refresh(request):
+    """Refreshes the value of the cgpa of the user on the dashboard by calculating the value
+    and the gpa value of the individual semester."""
     user = request.user
     user_results = list(user.results.order_by('result_id').all())
     prev_courses = list()
@@ -24,16 +26,26 @@ def refresh(request):
     
     for result in user_results:
         result_id = result.result_id
-        prev_courses += [(course.gradient, course.unit) for course in result.courses.all() if is_allowed(course, result_id)]
-        try:
-            gradients, units = list(zip(*prev_courses))
-        except ValueError:
-            # if `prev_courses` is empty i.e. none of the course passes validation
-            gradients, units = (), ()   # defaults to empty tuple
-        result.cgpa = sum(gradients)/ sum(units)
+        curr_courses = [(course.gradient, course.unit) for course in result.courses.all() if is_allowed(course, result_id)]
+        prev_courses += curr_courses
     
-    user.cgpa = result.cgpa
+        try:
+            gradients, units = list(zip(*prev_courses))     # getting the gradient and units of the previous courses in a separate list
+            c_gradients, c_units = list(zip(*curr_courses)) # getting the gradient and units of the current courses in a separate list
+        except ValueError:
+            # if `prev_courses` or `curr_courses` is empty i.e. none of the course passes validation
+            gradients = units = c_gradients = c_units = ()   # defaults to empty tuple
+        result.gpa = sum(c_gradients) / sum(c_units)
+        result.cgpa = sum(gradients) / sum(units)
+    
+    try:
+        user.cgpa = result.cgpa
+    except UnboundLocalError:   # i.e. `user_results` is empty thereby `result` variable is never defined
+        user.cgpa = 0.00    # reset the user result to 0.00
     user.save()
-    SemesterResult.objects.bulk_update(user_results, fields=('cgpa',))
+    SemesterResult.objects.bulk_update(user_results, fields=(
+        'cgpa',
+        'gpa',
+    ))
 
     return redirect(reverse('dashboard:dashboard'))
