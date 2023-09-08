5cb5e8c6b6332888825c9638a14ab8f144b0db51
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-09-08 11:18:10 +0100

    ✔: added url route for `Course` CRUD operations

--- results/urls.py
+++ results/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from .views import upload_result_view, CourseDetailView, CourseUpdateView
+from .views import (
+    upload_result_view,
+    CourseDetailView,
+    CourseUpdateView,
+    CourseDeleteView,
+)
 
 
 
@@ -11,4 +16,5 @@ urlpatterns = [
     path('upload/', upload_result_view, name='upload'),
     path('course/<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
     path('course/<int:pk>/edit/', CourseUpdateView.as_view(), name='course_edit'),
+    path('course/<int:pk>/delete/', CourseDeleteView.as_view(), name='course_delete'),
 ]
