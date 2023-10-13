c94dc60500b1b8bfaf12c4b640d94d81757a4dd0
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-10-13 03:04:12 +0100

    🟢: rewrote "build_files.sh" to always change `DEBUG` to True

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 # build_files.sh
-
+sed -i 's|DEBUG = True|DEBUG = False|g' ./portfolio/settings.py
+head -n 36 ./portfolio/settings.py | tail -n 1
 pip install -r requirements.txt
+python3.9 manage.py migrate
 python3.9 manage.py collectstatic --noinput
