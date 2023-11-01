a3a72872fa5fda08fc17e32aea58dc1d599dea04
Farouk Adegboyega <faroukadekunle001@gmail.com>
2023-11-01 15:27:11 +0100

    🐱‍👤: add DEBUG settings to env variable and remove the equivalent code in the `build_files`

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,4 @@
 # build_files.sh
-sed -i 's|DEBUG = True|DEBUG = False|g' ./portfolio/settings.py
 head -n 36 ./portfolio/settings.py | tail -n 1
 pip install -r requirements.txt
 python3.9 manage.py migrate
